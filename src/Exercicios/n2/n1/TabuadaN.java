943c2a8dbb92f62f8b3d8052bf1f553a476576d7
Luiz Felipe <luizphelip1998@gmail.com>
2025-04-13 21:18:52 -0300

    Resolvendo o exercício 1 do módulo 2 - Estruturas de Controle em Java

---
+++ src/Exercicios/n2/n1/TabuadaN.java
@@ -0,0 +1,16 @@
+package Exercicios.n2.n1;
+
+import java.util.Scanner;
+
+public class TabuadaN {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite um número: ");
+        int numero = scanner.nextInt();
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(numero + " * " + i + " = " + numero * i);
+        }
+    }
+}
