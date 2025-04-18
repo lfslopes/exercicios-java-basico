8d7470c1c10dab64f4eac100a9d40a9ae7273a94
Luiz Felipe <luizphelip1998@gmail.com>
2025-04-17 22:27:54 -0300

    Resolvendo o exercício 1 do módulo 4 - Herança e Polimorfismo em Java - Atualizando o método gerarRelatorioFinanceiro() da classe Gerente.java

--- src/Exercicios/n4/n2/SistemaEmpresa/Gerente.java
+++ src/Exercicios/n4/n2/SistemaEmpresa/Gerente.java
@@ -7,8 +7,17 @@ public class Gerente extends Funcionario {
         super(nome, email, senha, true);
     }
 
-    public String gerarRelatorioFinanceiro(List<Funcionario> funcionarios, List<Gerente> atendentes) {
-
+    public String gerarRelatorioFinanceiro(List<Vendedor> vendedores, List<Atendente> atendentes) {
+        int vendasTotais = 0;
+        double valorTotalEmCaixa = 0;
+        for (Vendedor vendedor : vendedores) {
+            vendasTotais += this.ConsultarVendas(vendedor);
+        }
+        for (Atendente atendente : atendentes) {
+            valorTotalEmCaixa += atendente.getValorEmCaixa();
+        }
+        return "Quantidade de vendas realizadas: " + vendasTotais + "\n" +
+               "Quantidade total em caixa: " + valorTotalEmCaixa + "\n";
     }
     public int ConsultarVendas (Vendedor vendedor) {
         return vendedor.ConsultarVendas();
