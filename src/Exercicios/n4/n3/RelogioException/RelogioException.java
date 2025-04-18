621f22edaedf057ebf401cf6bded0acb802406b2
Luiz Felipe <luizphelip1998@gmail.com>
2025-04-18 00:58:10 -0300

    Resolvendo o exercício 3 do módulo 4 - Herança e Polimorfismo em Java - Criando exception para relógios

---
+++ src/Exercicios/n4/n3/RelogioException/RelogioException.java
@@ -0,0 +1,7 @@
+package Exercicios.n4.n3.RelogioException;
+
+public class RelogioException extends RuntimeException {
+    public RelogioException(String message) {
+        super(message);
+    }
+}
